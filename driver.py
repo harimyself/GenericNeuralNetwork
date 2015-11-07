357df625b0ca4684bf619b602f56a1ac2ef5d48c
Hari <harimyself.b@gmail.com>
2015-11-07 15:38:57 -0800

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,15 +1,35 @@
+import numpy
+import theano
 
 from DataLoader import load_data
 from NN import NNetwork
+
+def create_y_vec(y, classes):
+	m = len(y)
+	for i in range(0, m):
+		if y[i]==0:
+			y[i] = 10
+		#print(y[i])
+	
+	y_vec = numpy.zeros(
+			(classes, m),
+			dtype=theano.config.floatX
+			)
+	
+	for i in range(0, m):
+		y_vec[y[i]-1][i] = 1
+
+	return y_vec
 
 def drive(input_layer_size, hidden_layer_size, classes, is_local):
 	
 	X, y = load_data('../data/train_sample.csv', is_local, need_split=False)
 
+	y_vec = create_y_vec(y, classes)
+	#print(y_vec)
 	network = NNetwork(input_layer_size, hidden_layer_size, 1, classes)
-	network.ignite(X, y)
-	
-	
+	network.ignite(X, y_vec)
+
 
 if __name__ == '__main__':
 	input_layer_size = 784
